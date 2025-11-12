edf4f1eed2293b0fa1baae0bb5e1a0642090fed1
AdzelFirestar <69654954+AdzelFirestar@users.noreply.github.com>
2025-11-11 18:38:24 -0800

    Update not-found.js

--- src/views/not-found.js
+++ src/views/not-found.js
@@ -1,24 +1,23 @@
+// ./src/views/not-found.js
 import React from 'react'
-
-import { Helmet } from 'react-helmet'
-
 import './not-found.css'
 
-const NotFound = (props) => {
+import Navigation from '../components/navigation'
+import Footer from '../components/footer'
+
+const NotFound = () => {
   return (
     <div className="not-found-container1">
-      <Helmet>
-        <title>404 - Not Found</title>
-      </Helmet>
-      <h3>OOPS! PAGE NOT FOUND</h3>
-      <div className="not-found-container2">
+      <Navigation />
+
+      <main className="not-found-container2">
         <h1 className="not-found-text2">404</h1>
-      </div>
-      <div className="not-found-container3">
-        <h2 className="not-found-text3">
-          WE ARE SORRY, BUT THE PAGE YOU REQUESTED WAS NOT FOUND
-        </h2>
-      </div>
+        <div className="not-found-container3">
+          <p className="not-found-text3">Oops! The page you’re looking for doesn’t exist.</p>
+        </div>
+      </main>
+
+      <Footer />
     </div>
   )
 }
